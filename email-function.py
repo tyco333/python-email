99311d9af67aed8d3b106a983bfe32f109385c90
tyco333 <34726618+tyco333@users.noreply.github.com>
2018-08-25 08:43:04 -0700

    Update email-function.py

--- email-function.py
+++ email-function.py
@@ -1,5 +1,6 @@
 import smtplib
 import time
+#Replace emailuser with your gmail username, emailpass with your gmail password, and emaildest with the email adress of the destination
 def sendemail(msg):
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.starttls()
